dc42371d4aa6b94d607e80c990b49bc4a360dfa7
Aitor Moreno <aitor@rojo2.com>
2017-11-24 23:25:45 +0100

    Add test

--- test/index.js
+++ test/index.js
@@ -1,7 +1,9 @@
+const clamp = require("../index.js");
+
 describe("clamp", function() {
 	it("should clamp values between min and max", function() {
 		expect(clamp(10,5,20)).to.be.equal(10);
-		expect(clamp(5,10,20)).to.be.equal(5);
+		expect(clamp(5,10,20)).to.be.equal(10);
 		expect(clamp(50,5,20)).to.be.equal(20);
 	});
 });
